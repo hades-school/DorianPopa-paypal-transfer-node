47671ca18e261e120f6d4191823c0b9acb3b159c
HADES <139430741+hades255@users.noreply.github.com>
2023-10-05 04:47:16 +0900

    credential tile

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const paypal = require("paypal-rest-sdk");
 
 const app = express();
+app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
